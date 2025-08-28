b3238c0ed464bd96990e790b8b939705858293c6
Merkif <nokiaman1997@gmail.com>
2025-08-29 02:24:38 +0300

    Рефакторинг анимации вступительной секции

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,26 +1,26 @@
-import MenuManager from "../functions/MenuManager.js";
+import MenuManager from "../functions/menu-manager.js";
 
 const page = document.documentElement;
 const menu = new MenuManager({
-	menu: '.menu__container',
-	menuOpenClass: 'menu__container--open',
+	menu: ".menu__container",
+	menuOpenClass: "menu__container--open",
 	onMenuOpen() {
-		page.classList.remove('page--smooth');
+		page.classList.remove("page--smooth");
 	},
 	onMenuClose() {
 		setTimeout(() => {
-			page.classList.add('page--smooth');
+			page.classList.add("page--smooth");
 		}, 500);
 
 		closeAllDropdowns(menu);
-	}
+	},
 });
 
 function closeAllDropdowns(menu) {
-	const dropdowns = menu.menuElement.querySelectorAll('.menu__item--open');
-	dropdowns.forEach(dropdown => {
-		if(dropdown.classList.contains('menu__item--open')) {
-			dropdown.classList.remove('menu__item--open');
+	const dropdowns = menu.menuElement.querySelectorAll(".menu__item--open");
+	dropdowns.forEach((dropdown) => {
+		if (dropdown.classList.contains("menu__item--open")) {
+			dropdown.classList.remove("menu__item--open");
 		}
 	});
 }
--- src/js/components/section-hero-anim.js
+++ src/js/components/section-hero-anim.js
@@ -3,147 +3,66 @@ import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
-function getDistanceToSectionBottom(element, section) {
-	const elRect = element.getBoundingClientRect();
-	const sectionRect = section.getBoundingClientRect();
-	return sectionRect.bottom - elRect.top;
-}
+const sectionHero = document.querySelector(".section-hero");
+const sectionHeroContent = sectionHero.querySelector(".section-hero__content");
+const sectionHeroTitle = sectionHero.querySelector(".section-hero__title");
+const sectionHeroSubtitle = sectionHero.querySelector(
+	".section-hero__subtitle"
+);
+const tileFirst = sectionHero.querySelector(".section-hero .tile--1");
+const tileSecond = sectionHero.querySelector(".section-hero .tile--2");
+const tileThird = sectionHero.querySelector(".section-hero .tile--3");
+const tileFourth = sectionHero.querySelector(".section-hero .tile--4");
+const tileFifth = sectionHero.querySelector(".section-hero .tile--5");
+const tileGroup = sectionHero.querySelector(".section-hero .tile-group");
 
-function getDistance(element, section) {
-	const elRect = element.getBoundingClientRect();
-	const sectionRect = section.getBoundingClientRect();
-	return sectionRect.bottom - elRect.bottom;
-}
-
-let tl = gsap.timeline();
-const sectionHeroContent = document.querySelector(".section-hero__content");
-const sectionHeroTitle = document.querySelector(".section-hero__title");
-const sectionHeroSubtitle = document.querySelector(".section-hero__subtitle");
-
-const tiles = [
-	{ el: document.querySelector(".tile--1"), start: "clamp(center 90%)" },
-	{ el: document.querySelector(".tile--2"), start: "clamp(center 75%)" },
-	{ el: document.querySelector(".tile--3"), start: "clamp(center 85%)" },
-];
-
-tiles.forEach(({ el, start }) => {
-	if (!el) return;
-	gsap.to(el, {
-		"--height": () =>
-			getDistanceToSectionBottom(el, sectionHeroContent) + "px",
-		ease: "linear",
-		scrollTrigger: {
-			trigger: el,
-			scrub: true,
-			start,
-			invalidateOnRefresh: true,
-			end: () =>
-				"+=" + getDistanceToSectionBottom(el, sectionHeroContent),
-		},
-	});
+const tl = gsap.timeline({
+	ease: "none",
+	scrollTrigger: {
+		trigger: ".section-hero",
+		start: "clamp(center 90%)",
+		end: "+=1000",
+		scrub: true,
+	},
 });
 
-if (sectionHeroTitle) {
-	gsap.fromTo(
-		sectionHeroTitle,
-		{ yPercent: 0 },
-		{
-			yPercent: -40,
-			scrollTrigger: {
-				trigger: sectionHeroTitle,
-				scrub: true,
-				start: "clamp(top 80%)",
-				end: "clamp(bottom 10%)",
-			},
-		}
-	);
-}
+gsap.set(tileFifth, { opacity: "0" });
 
-if (sectionHeroSubtitle) {
-	gsap.fromTo(
-		sectionHeroSubtitle,
-		{ yPercent: 0 },
+tl.to(tileFirst, {
+	keyframes: {
+		"50%": { height: "60%" },
+		"100%": { height: "100%" },
+	},
+})
+	.to(
+		tileSecond,
 		{
-			yPercent: -90,
-			scrollTrigger: {
-				trigger: sectionHeroSubtitle,
-				scrub: true,
-				start: "clamp(top 80%)",
-				end: "clamp(bottom 40%)",
+			keyframes: {
+				"50%": { height: "40%" },
+				"100%": { height: "100%" },
 			},
-		}
-	);
-}
-
-if (sectionHeroContent) {
-	gsap.fromTo(
-		sectionHeroContent,
-		{ yPercent: 0 },
-		{
-			yPercent: -10,
-			scrollTrigger: {
-				trigger: sectionHeroContent,
-				scrub: true,
-				start: "clamp(top 80%)",
-				end: "clamp(bottom 60%)",
-			},
-		}
-	);
-
-	tl.to(".section-hero .tile-group", {
-		y: () =>
-			getDistance(
-				document.querySelector(".section-hero .tile-group"),
-				document.querySelector(".section-hero")
-			) + "px",
-		ease: "linear",
-		scrollTrigger: {
-			trigger: ".section-hero .tile--5",
-			scrub: true,
-			start: "clamp(center 100.5%)",
-			invalidateOnRefresh: true,
-			end: () =>
-				"+=" +
-				getDistance(
-					document.querySelector(".section-hero .tile-group"),
-					document.querySelector(".section-hero")
-				),
 		},
-	});
-
-	tl.to(".section-hero .tile--4", {
-		height: 124,
-		scrollTrigger: {
-			trigger: ".section-hero .tile--4",
-			scrub: true,
-			start: "clamp(center 89.5%)",
-			end: "clamp(center 80%)",
-		},
-	});
-
-	tl.fromTo(
-		".section-hero .tile--5",
-		{ opacity: 0, height: 82 },
+		0
+	)
+	.to(
+		tileThird,
 		{
-			opacity: 1,
-			height: 134,
-			scrollTrigger: {
-				trigger: ".section-hero .tile--4",
-				scrub: true,
-				start: "clamp(center 89.5%)",
-				end: "clamp(center 80%)",
+			keyframes: {
+				"50%": { height: "45%", "--cp": "0%" },
+				"100%": { height: "100%", "--cp": "100%" },
 			},
-		}
-	);
-
-	tl.to(".section-hero .tile--4", {
-		scaleY: 8,
-		ease: "linear",
-		scrollTrigger: {
-			trigger: ".section-hero .tile--5",
-			scrub: true,
-			start: "clamp(center 80.5%)",
-			end: "clamp(center 50%)",
 		},
-	});
-}
+		0
+	)
+	.to(sectionHeroSubtitle, { y: -100 }, 0)
+	.to(sectionHeroTitle, { y: -100 }, 0)
+	.to(sectionHeroContent, { y: -70 }, 0)
+	.to(
+		tileFifth,
+		{
+			keyframes: {
+				"50%": { opacity: 1, height: "134px" },
+			},
+		},
+		0
+	);
